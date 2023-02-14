8e6e1a22b1ac62c24f60c407f083df40880350a5
dahui-sharon-kim <dahui.s.kim@gmail.com>
2023-02-14 17:50:35 +0900

    feat: firebase auth configuration

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,3 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const database = getFirestore();
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
