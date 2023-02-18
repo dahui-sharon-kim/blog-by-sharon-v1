12891ecd4c18a64a95e6a2f3deda583d6817c6e7
dahui-sharon-kim <dahui.s.kim@gmail.com>
2023-02-19 01:56:34 +0900

    feat: global style text color gray

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,13 @@
 export const lightMode = {
   body: "var(--background-lightmode)",
   text: "var(--black)",
+  textGray: "var(--gray1)",
   bg: "var(--white-true)",
 };
 
 export const darkMode = {
   body: "var(--background-darkmode)",
   text: "var(--white)",
+  textGray: "var(--lightgray4)",
   bg: "var(--gray3)",
 };
